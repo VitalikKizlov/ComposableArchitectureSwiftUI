87eb7e6858a74722799ecf26e3d8e1a3f3ab1e97
Vitalii Kizlov <vitalik2602@gmail.com>
2021-12-03 10:01:29 +0200

    Initial Commit

---
+++ ComposableArchitecture/ComposableArchitectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableArchitectureApp.swift
+//  ComposableArchitecture
+//
+//  Created by Vitalii Kizlov on 03.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct ComposableArchitectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ComposableArchitectureUITests/ComposableArchitectureUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ComposableArchitectureUITestsLaunchTests.swift
+//  ComposableArchitectureUITests
+//
+//  Created by Vitalii Kizlov on 03.12.2021.
+//
+
+import XCTest
+
+class ComposableArchitectureUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
